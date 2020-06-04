9c5f510a86b3158d012f470f811010f2047884a5
Ping <pingao2019@outlook.com>
2020-06-04 14:13:38 -0400

    add notes

--- src/player.py
+++ src/player.py
@@ -1,21 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-class Player():
-    def __init__(self, name, current_room, item):
-        self.name= name
-        self.current_room= current_room
-        self.item =item
-
-    def __str__(self):
-        return f"{self.name}, you are at {self.room.name} now. \n{self.room.description}"
+class Player:
+    def __init__(self, location):
+        self.location = location
+        
+    # def __str__(self):
+    #     return f"{self.name}, you are at {self.room.name} now. \n{self.room.description}"
 
 
-players={'captain': Room['floyer'],
-'monster':Room['treasure']}
+# players=[{'captain': Room['floyer']},
+# {'monster':Room['treasure']}]
 
 
-class monster:
-    def __init__(self):
-        pass
-    def on_attack(self):
-        return 
+# class monster:
+#     def __init__(self):
+#         pass
+#     def on_attack(self):
+#         return 
--- src/room.py
+++ src/room.py
@@ -1,19 +1,14 @@
 
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
 class Room:
-    def __init__ (self, name, description, n_to, s_to, e_to, w_to):
-        self.name = name
+    def __init__(self, name, description):
+        self.name = name 
         self.description = description
-        self.n_to=n_to
-        self.s_to= s_to
-        self.e_to= e_to
-        self.w_to= w_to
 
-  
     def __str__(self):
-        return f"This is {self.name}, and {self.description}"
-
+        return f"{self.name}: {self.description}"
 
 
 item_list= {'sword': Item("get "),
